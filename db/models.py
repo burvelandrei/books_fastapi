248e0bcd5276af52aac50097adacc9fed0291f53
Andrei Burvel <burvelandrei@gmail.com>
2025-02-11 14:13:07 +0300

    V 1.2 добавлены колонки в моодели таблиц

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, relationship
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Date
 from datetime import datetime
 
 
@@ -18,6 +18,7 @@ class Book(Base, BaseMixin):
 
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
+    date_publication = Column(Date, nullable=True)
     author_id = Column(Integer, ForeignKey("author.id", ondelete="CASCADE"), nullable=False)
 
     author = relationship("Author", back_populates="books")
@@ -27,5 +28,7 @@ class Author(Base, BaseMixin):
     __tablename__ = "author"
 
     name = Column(String(100), nullable=False)
+    alias = Column(String(100), nullable=True)
+    country = Column(String(100), nullable=True)
 
     books = relationship("Book", back_populates="author")
